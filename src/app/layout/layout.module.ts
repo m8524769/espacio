7a6d47b4c2f135c205190652b7b352f8c5c61f39
m8524769 <m8524769@163.com>
2019-12-10 21:06:21 +0800

    Update Angular to 9.0.0-rc5

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {
-  MatSidenavModule,
-  MatToolbarModule,
-  MatExpansionModule,
-  MatIconModule,
-  MatListModule,
-  MatButtonModule,
-  MatMenuModule,
-  MatDividerModule,
-  MatTooltipModule,
-  MatCardModule,
-  MatBottomSheetModule,
-  MatInputModule,
-  MatSelectModule,
-} from '@angular/material';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { LayoutComponent } from './layout.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
