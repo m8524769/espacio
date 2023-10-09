fe76706104488b1a54147aa697315f69456cd3e1
yk <m8524769@163.com>
2023-10-09 20:27:49 +0800

    chore: init

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# EspacioElectron
+# ESPACIO
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.5.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'espacio-electron'`, () => {
+  it(`should have as title 'espacio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('espacio-electron');
+    expect(app.title).toEqual('espacio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('espacio-electron app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('espacio app is running!');
   });
 });
